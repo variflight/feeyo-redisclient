87b8f585d2299e44db7b84029e1341074d049462
zhuam <zhuam@foxmail.com>
2018-07-05 17:44:31 +0800

    rt

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -7,14 +7,13 @@ password = 'pwd05'
 
 r = redis.Redis(host=host, port=port, password=password, decode_responses=True)
 
-# kpush
+# kpush topic content
 args = ['test02','python_test']
 resp1 = r.execute_command('kpush', *args)
 print resp1
 
-# kpop
+# kpop topic
 resp2 = r.execute_command('kpop', "test02")
 print resp2
-
 
 
